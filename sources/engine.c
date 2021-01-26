3aac06da7580980dac8684eeebfd3d6790a973dd
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-01-26 09:29:05 +0100

    Update and rename engine.cpp to engine.c

---
+++ sources/engine.c
@@ -0,0 +1,14 @@
+#include <sdtio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <gmp.h>
+
+/** Important for final multiplication */
+#define A   13591409
+#define B   545140134
+#define C   640320
+#define D   12
+
